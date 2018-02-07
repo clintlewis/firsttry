e4a18d0eb29b1055e573743350a2eeb727a14a16
Clint Lewis <clint.lewis.pdx@gmail.com>
2018-02-06 19:24:20 -0800

    test

---
+++ addition.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# print('Hey there' + '\nSilly don\'t')
+# print("I love beer")
+
+#x = 3
+#f = 3.14159
+#name = "Python"
+#print(x)
+#print(f)
+#print(name)
+#combination = name + " " + name
+#print(combination)
+#print(f + f)
+#print("I added " + str(x) + " and " + str(f))
+
+n = 7
+s = "My lucky number is %d, what is yours? %d" % (n,n)
+print(s)
